8e96fc125a782c2de6ad074fc3b97a57d737fc74
Jakub <jaresz.git@listy.internetowe.pl>
2016-04-27 12:17:45 +0200

    Popularne akcesory do traitów

--- src/AppBundle/Entity/Traits/BaseEntityTrait.php
+++ src/AppBundle/Entity/Traits/BaseEntityTrait.php
@@ -37,5 +37,89 @@ trait BaseEntityTrait {
      */
     protected $rapas;
     
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+    
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Payment
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    
+        return $this;
+    }
+    
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+    
+    /**
+     * Set created
+     *
+     * @param \DateTime $created
+     *
+     * @return Payment
+     */
+    public function setCreated($created)
+    {
+        $this->created = $created;
+    
+        return $this;
+    }
+    
+    /**
+     * Get created
+     *
+     * @return \DateTime
+     */
+    public function getCreated()
+    {
+        return $this->created;
+    }
+    
+    /**
+     * Set updated
+     *
+     * @param \DateTime $updated
+     *
+     * @return Payment
+     */
+    public function setUpdated($updated)
+    {
+        $this->updated = $updated;
+    
+        return $this;
+    }
+    
+    /**
+     * Get updated
+     *
+     * @return \DateTime
+     */
+    public function getUpdated()
+    {
+        return $this->updated;
+    }
+    /**
+     * Constructor
+     */
     
 }
